3a7706443104103ad986ed3dc9475fae15c41e3b
gaconkzk <gaconkzk@gmail.com>
2021-10-18 14:18:05 +0700

    correct base output for docs

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -4,8 +4,10 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flyui/',
   build: {
     outDir: '../../docs',
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
