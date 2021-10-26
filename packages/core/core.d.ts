b3c63c6d98771c3a71eb9c37d02dab5eaf208112
gaconkzk <gaconkzk@gmail.com>
2021-10-26 10:34:24 +0700

    svelte wip

--- packages/core/core.d.ts
+++ packages/core/core.d.ts
@@ -1,2 +1,3 @@
 declare module '@gaconkzk/core/tw-nm'
 declare module '@gaconkzk/core/theme'
+declare module '@gaconkzk/core/lib-inject'
--- packages/flyui-svelte/vite.config.js
+++ packages/flyui-svelte/vite.config.js
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+import WindiCSS from 'vite-plugin-windicss'
+import * as path from 'path'
+
+const __dirname = path.resolve()
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'flyui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: [
+        {
+          format: 'esm',
+          esModule: true,
+          globals: {
+            vue: 'Vue',
+          },
+          exports: 'named',
+        },
+        // This umd config not tested and must be put after esm - when put before - the code splitting not work anymore
+        {
+          format: 'umd',
+          interop: 'esModule',
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            vue: 'Vue',
+          },
+          // For dynamic import code (svg icons for now)
+          inlineDynamicImports: true,
+          exports: 'named',
+        },
+      ],
+    },
+  },
+  assetsInclude: ['**/*.svg'],
+  plugins: [svelte(), WindiCSS()],
 })
