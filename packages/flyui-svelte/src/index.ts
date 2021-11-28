cd079fc3d580686d959ee0070d2e8a555a72d2be
gaconkzk <gaconkzk@gmail.com>
2021-11-28 08:09:01 +0700

    try export icon and commonize icons

--- packages/flyui-svelte/src/index.ts
+++ packages/flyui-svelte/src/index.ts
@@ -1,3 +1,4 @@
 export { default as FButton } from '@/lib/FButton.svelte'
 export { default as FIcon } from '@/lib/FIcon.svelte'
 export { default as scrollable } from '@/lib/useScrollable'
+export { theme } from '@gaconkzk/core/utils/theme'
--- packages/flyui-svelte/src/lib/names.ts
+++ packages/flyui-svelte/src/lib/names.ts
@@ -1,12 +1,12 @@
 const icons: { [key: string]: Promise<typeof import('*.svg?raw')> } = {
-  flies_bag: import('@/assets/icons/svg/flies_bag.svg?raw'),
-  fly_filled: import('@/assets/icons/svg/fly_filled.svg?raw'),
-  fly_logo: import('@/assets/icons/svg/fly_logo.svg?raw'),
-  fly_outline: import('@/assets/icons/svg/fly_outline.svg?raw'),
-  fly: import('@/assets/icons/svg/fly.svg?raw'),
-  s_fly_filled: import('@/assets/icons/svg/s_fly_filled.svg?raw'),
-  s_fly: import('@/assets/icons/svg/s_fly.svg?raw'),
-  'chevron-down': import('@/assets/icons/svg/chevron-down.svg?raw'),
+  flies_bag: import('@gaconkzk/core/assets/icons/svg/flies_bag.svg?raw'),
+  fly_filled: import('@gaconkzk/core/assets/icons/svg/fly_filled.svg?raw'),
+  fly_logo: import('@gaconkzk/core/assets/icons/svg/fly_logo.svg?raw'),
+  fly_outline: import('@gaconkzk/core/assets/icons/svg/fly_outline.svg?raw'),
+  fly: import('@gaconkzk/core/assets/icons/svg/fly.svg?raw'),
+  s_fly_filled: import('@gaconkzk/core/assets/icons/svg/s_fly_filled.svg?raw'),
+  s_fly: import('@gaconkzk/core/assets/icons/svg/s_fly.svg?raw'),
+  'chevron-down': import('@gaconkzk/core/assets/icons/svg/chevron-down.svg?raw'),
 }
 export const get = (name: string) => {
   return icons[name] ?? icons.flies_bag
