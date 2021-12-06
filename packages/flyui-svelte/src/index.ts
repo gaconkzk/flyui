901e64fe9a7b27d9810d5cc646ac56cdb1d835b6
gaconkzk <gaconkzk@gmail.com>
2021-12-06 10:22:27 +0700

    variables and nmstyle import

--- packages/flyui-svelte/src/index.ts
+++ packages/flyui-svelte/src/index.ts
@@ -1,4 +1,9 @@
+// common ones from core
+import '@gaconkzk/core/styles/variables.scss'
+import '@gaconkzk/core/styles/neumophism.scss'
+
 export { default as FButton } from '@/lib/FButton.svelte'
 export { default as FIcon } from '@/lib/FIcon.svelte'
 export { default as scrollable } from '@/lib/useScrollable'
 export { theme } from '@gaconkzk/core/utils/theme'
+export { default as cssVariables} from '@gaconkzk/core/styles/variables.scss'
