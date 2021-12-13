c47fba519d57fb8fb49ff61ec72f127d9003150d
gaconkzk <gaconkzk@gmail.com>
2021-12-13 09:47:14 +0700

    style -

--- packages/flyui-sveltex/src/lib/index.ts
+++ packages/flyui-sveltex/src/lib/index.ts
@@ -1,6 +1,6 @@
 // common ones from core
 import 'virtual:windi.css'
-import '@/styles/index.scss'
+import './styles/index.scss'
 
 export { default as FButton } from '$lib/components/FButton.svelte'
 export { default as FIcon } from '$lib/components/FIcon.svelte'
