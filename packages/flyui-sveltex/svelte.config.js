21d8d0a699ea410adb7cff2489317ecb1a9b7b65
gaconkzk <gaconkzk@gmail.com>
2021-12-14 09:21:07 +0700

    missed

--- packages/flyui-sveltex/svelte.config.js
+++ packages/flyui-sveltex/svelte.config.js
@@ -15,6 +15,9 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     vite: {
+      optimizeDeps: {
+        include: ['color'],
+      },
       plugins: [WindiCSS()],
     },
   },
